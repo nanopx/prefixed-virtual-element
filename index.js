e2d0fed2ea02b458e4731e66f838438406c10a38
吉武篤司 <0nanopx@gmail.com>
2016-01-13 15:25:08 +0900

    removed magic-virtual-element from deps

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-import element from 'magic-virtual-element';
+import { string } from 'to-style'
+import classNames from 'classnames'
 import Prefixer from 'inline-style-prefixer';
 import componentType from 'component-type';
 
-export default function magicPrefixedElement(type, attributes, ...children) {
-  if (componentType(attributes.style) === 'object') {
-    attributes.style = Prefixer.prefixAll(attributes.style);
+export default function prefixedVirtualElement(type, attributes, ...children) {
+  let vnode = element(type, attributes, ...children)
+
+  if (componentType(vnode.attributes.class) === 'array') {
+    vnode.attributes.class = classNames.apply(null, vnode.attributes.class)
   }
-  return element(type, attributes, ...children);
+
+  if (componentType(vnode.attributes.class) === 'object') {
+    vnode.attributes.class = classNames(vnode.attributes.class)
+  }
+
+  if (componentType(vnode.attributes.style) === 'object') {
+    const prefixedStyle = Prefixer.prefixAll(vnode.attributes.style)
+    vnode.attributes.style = string(prefixedStyle)
+  }
+
+  if (!vnode.attributes.hidden) {
+    delete vnode.attributes.hidden
+  }
+
+  return vnode
 }
